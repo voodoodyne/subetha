b53f0fec1becf16d1dc2375f0a0b4569fff35c39
Jon Scott Stevens <latchkey@gmail.com>
2006-05-08 23:04:12 +0000

    i18n now seems to work.
    git-svn-id: https://subetha.googlecode.com/svn/trunk@296 68df8246-1725-11de-bcda-2bb9902f8029

---
+++ frontend/src/org/subethamail/web/util/SetRequestCharsetFilter.java
@@ -0,0 +1,40 @@
+/*
+ * $Id$
+ * $URL$
+ */
+
+package org.subethamail.web.util;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * For i18n reasons, we need to set the character encoding on 
+ * all requests into the server.
+ * 
+ * @author Jon Stevens
+ */
+public class SetRequestCharsetFilter extends AbstractFilter
+{
+	/** */
+	private static Log log = LogFactory.getLog(SetRequestCharsetFilter.class);
+	
+	/**
+	 */
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+		throws IOException, ServletException
+	{
+		if (log.isDebugEnabled())
+			log.debug("*** Starting filter for " + request.getRequestURI());
+		
+		if (request.getCharacterEncoding() == null)
+			request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+}

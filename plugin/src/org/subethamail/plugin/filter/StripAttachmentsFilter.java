d9fea817258f3b7a3152023c4e8bede4604e6d9d
Jon Scott Stevens <latchkey@gmail.com>
2006-05-17 16:32:53 +0000

    wrong filter import
    git-svn-id: https://subetha.googlecode.com/svn/trunk@400 68df8246-1725-11de-bcda-2bb9902f8029

--- plugin/src/org/subethamail/plugin/filter/StripAttachmentsFilter.java
+++ plugin/src/org/subethamail/plugin/filter/StripAttachmentsFilter.java
@@ -14,6 +14,7 @@ import org.apache.commons.logging.LogFactory;
 import org.jboss.annotation.ejb.Service;
 import org.jboss.annotation.security.SecurityDomain;
 import org.subethamail.common.SubEthaMessage;
+import org.subethamail.core.plugin.i.Filter;
 import org.subethamail.core.plugin.i.FilterContext;
 import org.subethamail.core.plugin.i.FilterParameter;
 import org.subethamail.core.plugin.i.HoldException;
@@ -21,7 +22,6 @@ import org.subethamail.core.plugin.i.IgnoreException;
 import org.subethamail.core.plugin.i.helper.FilterParameterImpl;
 import org.subethamail.core.plugin.i.helper.GenericFilter;
 import org.subethamail.core.plugin.i.helper.Lifecycle;
-import com.sun.org.apache.xalan.internal.xsltc.dom.Filter;
 
 /**
  * This filter removes all attachments greater than a certain size

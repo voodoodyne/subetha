ec4b9f60ae746b25d46f2975065d6821f8182457
Scott Hernandez <scotthernandez@gmail.com>
2006-05-22 16:39:47 +0000

    Suppress unused warnings in eclipse
    git-svn-id: https://subetha.googlecode.com/svn/trunk@489 68df8246-1725-11de-bcda-2bb9902f8029

--- core/src/org/subethamail/core/util/DetachmentContentHandler.java
+++ core/src/org/subethamail/core/util/DetachmentContentHandler.java
@@ -31,6 +31,7 @@ import org.apache.commons.logging.LogFactory;
 public class DetachmentContentHandler implements DataContentHandler
 {
 	/** */
+	@SuppressWarnings("unused")
 	private static Log log = LogFactory.getLog(DetachmentContentHandler.class);
 
 	/*

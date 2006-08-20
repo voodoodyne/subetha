aaea8bf681fc55f936044363aacb027f3ace8dac
Jeff Schnitzer <jeff@infohazard.org>
2006-08-20 00:54:48 +0000

    Cleanup service now runs daily by itself.
    git-svn-id: https://subetha.googlecode.com/svn/trunk@728 68df8246-1725-11de-bcda-2bb9902f8029

--- core/src/org/subethamail/core/admin/CleanupBean.java
+++ core/src/org/subethamail/core/admin/CleanupBean.java
@@ -5,8 +5,11 @@
 
 package org.subethamail.core.admin;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.annotation.security.RunAs;
 
@@ -39,20 +42,58 @@ public class CleanupBean extends EntityManipulatorBean implements CleanupManagem
 	/** Keep held messages around for 7 days */
 	public static final long MAX_HELD_MSG_AGE_MILLIS = 1000 * 60 * 60 * 24 * 7;
 	
-	/*
-	 * (non-Javadoc)
+	/** */
+	class CleanupTask extends TimerTask
+	{
+		public void run()
+		{
+			cleanup();
+		}
+	}
+	
+	/**
+	 * Timer used to schedule the service event.
+	 */
+	Timer timer = new Timer("Cleanup", false);
+	
+	/* (non-Javadoc)
 	 * @see org.subethamail.core.admin.CleanupManagement#start()
 	 */
 	public void start() throws Exception
 	{
+		log.info("Starting cleanup service");
+		
+		// Schedule rebuilds to occur nightly at 4am, plus some random slop time.
+		// The slop time makes this play nicer in a clustered environment.  Really
+		// this should be a HA singleton service, but that would require running
+		// JBoss in a clustered configuration even on a single box.
+		Calendar next = Calendar.getInstance();
+
+		next.set(Calendar.HOUR_OF_DAY, 4);
+		next.set(Calendar.MINUTE, (int)(60 * Math.random()));
+		next.set(Calendar.SECOND, (int)(60 * Math.random()));
+		next.set(Calendar.MILLISECOND, (int)(1000 * Math.random()));
+
+		Calendar now = Calendar.getInstance();
+		
+		if (now.after(next))
+			next.add(Calendar.DAY_OF_YEAR, 1);
+		
+		final long millisInDay = 1000 * 60 * 60 * 24;
+		
+		this.timer.scheduleAtFixedRate(new CleanupTask(), next.getTime(), millisInDay);
+		
+		log.info("Cleanup will occur daily starting at " + next.getTime());
 	}
 	
-	/*
-	 * (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see org.subethamail.core.admin.CleanupManagement#stop()
 	 */
 	public void stop() throws Exception
 	{
+		log.info("Stopping cleanup service");
+		
+		this.timer.cancel();
 	}
 	
 	/*
@@ -75,6 +116,8 @@ public class CleanupBean extends EntityManipulatorBean implements CleanupManagem
 		if (log.isDebugEnabled())
 			log.debug("Purging held subscriptions older than " + cutoff);
 		
+		int count = 0;
+		
 		List<SubscriptionHold> holds = this.em.findHeldSubscriptionsOlderThan(cutoff);
 		for (SubscriptionHold hold: holds)
 		{
@@ -82,7 +125,12 @@ public class CleanupBean extends EntityManipulatorBean implements CleanupManagem
 				log.debug("Deleting obsolete hold: " + hold);
 			
 			this.em.remove(hold);
+			count++;
 		}
+		
+		if (count > 0)
+			if (log.isInfoEnabled())
+				log.info(count + " obsolete subscription holds removed");
 	}
 	
 	/**
@@ -95,6 +143,8 @@ public class CleanupBean extends EntityManipulatorBean implements CleanupManagem
 		if (log.isDebugEnabled())
 			log.debug("Purging held mail older than " + cutoff);
 		
+		int count = 0;
+		
 		List<Mail> holds = this.em.findHeldMailOlderThan(cutoff);
 		for (Mail hold: holds)
 		{
@@ -102,6 +152,12 @@ public class CleanupBean extends EntityManipulatorBean implements CleanupManagem
 				log.debug("Deleting obsolete hold: " + hold);
 			
 			this.em.remove(hold);
+
+			count++;
 		}
+		
+		if (count > 0)
+			if (log.isInfoEnabled())
+				log.info(count + " obsolete message holds removed");
 	}
 }

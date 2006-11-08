2f3078c6b95775a4f5cbd6a7464a5aac92c3340d
Jon Scott Stevens <latchkey@gmail.com>
2006-11-08 20:22:31 +0000

    i'm tired of explaining to people that they need to login to see the content.
    git-svn-id: https://subetha.googlecode.com/svn/trunk@865 68df8246-1725-11de-bcda-2bb9902f8029

--- entity/src/org/subethamail/entity/i/PermissionException.java
+++ entity/src/org/subethamail/entity/i/PermissionException.java
@@ -22,8 +22,11 @@ public class PermissionException extends Exception
 	 */
 	public PermissionException(Permission needed)
 	{
-		super("Requires permission " + needed);
-		
+		super("Requires permission '" + needed.getPretty() + "'. " + 
+				"This means that you probably need to login first in order to perform an action. " +
+				"If you do not remember your password, click the 'forgot' link at the top of the " +
+				"page to have your password sent to you.");
+
 		this.needed = needed;
 	}
 	

bac329e909efcca568b283c37ad33b9e405a5d3c
Jeff Schnitzer <jeff@infohazard.org>
2007-04-19 20:54:48 +0000

    Fixed a JDK5/6 incompatibility issue with package-info.java
    git-svn-id: https://subetha.googlecode.com/svn/trunk@947 68df8246-1725-11de-bcda-2bb9902f8029

--- entity/src/org/subethamail/entity/Config.java
+++ entity/src/org/subethamail/entity/Config.java
@@ -20,6 +20,15 @@ import org.hibernate.annotations.Columns;
 import org.hibernate.annotations.Type;
 import org.subethamail.entity.i.Validator;
 
+// Moved this here from package-info.java to fix JDK5/6 incompatibility problem
+// For some reason package-info.java built with JDK6 doesn't work on JDK5
+@org.hibernate.annotations.TypeDefs({
+	@org.hibernate.annotations.TypeDef(
+		name="anyImmutable",
+		typeClass=org.subethamail.entity.type.AnyImmutableType.class            
+	)
+})
+
 /**
  * Contains a sitewide config parameter, key and value.
  * 

da7b802187cf91423ab8dfc57632ee24ade9d598
Jeff Schnitzer <jeff@infohazard.org>
2009-03-31 08:29:27 +0000

    Working on remoting and the unit tests.
    git-svn-id: https://subetha.googlecode.com/svn/branches/resin@1026 68df8246-1725-11de-bcda-2bb9902f8029

--- rtest/src/org/subethamail/rtest/util/BeanMixin.java
+++ rtest/src/org/subethamail/rtest/util/BeanMixin.java
@@ -5,31 +5,20 @@
 
 package org.subethamail.rtest.util;
 
-import java.security.Principal;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
+import java.net.MalformedURLException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jboss.security.SecurityAssociation;
-import org.jboss.security.SimplePrincipal;
 import org.subethamail.core.acct.i.AccountMgr;
-import org.subethamail.core.acct.i.AccountMgrRemote;
 import org.subethamail.core.admin.i.Admin;
-import org.subethamail.core.admin.i.AdminRemote;
 import org.subethamail.core.admin.i.ListWizard;
-import org.subethamail.core.admin.i.ListWizardRemote;
 import org.subethamail.core.admin.i.Plumber;
-import org.subethamail.core.admin.i.PlumberRemote;
 import org.subethamail.core.injector.i.Injector;
-import org.subethamail.core.injector.i.InjectorRemote;
 import org.subethamail.core.lists.i.Archiver;
-import org.subethamail.core.lists.i.ArchiverRemote;
 import org.subethamail.core.lists.i.ListMgr;
-import org.subethamail.core.lists.i.ListMgrRemote;
 import org.subethamail.core.search.i.Indexer;
-import org.subethamail.core.search.i.IndexerRemote;
+
+import com.caucho.hessian.client.HessianProxyFactory;
 
 /**
  * This class makes it easy to obtain and use the various
@@ -46,28 +35,9 @@ public class BeanMixin
 	@SuppressWarnings("unused")
 	private static Log log = LogFactory.getLog(BeanMixin.class);
 
-	private AccountMgr accountMgr;
-	private Admin admin;
-	private ListMgr listMgr;
-	private ListWizard listWizard;
-	private Indexer indexer;
-	private Injector injector;
-	private Archiver archiver;
-	private Plumber plumber;
-	
-	
 	/** */
 	public BeanMixin() throws Exception
 	{
-		Context ctx = new InitialContext();
-		this.admin = (Admin)ctx.lookup(AdminRemote.JNDI_NAME);
-		this.accountMgr = (AccountMgr)ctx.lookup(AccountMgrRemote.JNDI_NAME);
-		this.listMgr = (ListMgr)ctx.lookup(ListMgrRemote.JNDI_NAME);
-		this.listWizard = (ListWizard)ctx.lookup(ListWizardRemote.JNDI_NAME);
-		this.indexer = (Indexer)ctx.lookup(IndexerRemote.JNDI_NAME);
-		this.injector = (Injector)ctx.lookup(InjectorRemote.JNDI_NAME);
-		this.archiver = (Archiver)ctx.lookup(ArchiverRemote.JNDI_NAME);
-		this.plumber = (Plumber)ctx.lookup(PlumberRemote.JNDI_NAME);
 	}
 	
 	/** If this is null, clears all credentials */
@@ -79,74 +49,69 @@ public class BeanMixin
 	/**
 	 * Establish administrator credentials
 	 */
-	public void establish()
+	public Object getInterface(Class clazz)
 	{
-		if (this.getPrincipalName() == null)
+		HessianProxyFactory fact = new HessianProxyFactory();
+		if (this.getPrincipalName() != null)
 		{
-	        SecurityAssociation.setPrincipal(null);
-	        SecurityAssociation.setCredential(null);
+			fact.setUser(this.getPrincipalName());
+			fact.setPassword(this.getPassword());
 		}
-		else
+		
+		String url = "http://localhost:8080/api/" + clazz.getSimpleName();
+		
+		try
 		{
-			Principal p = new SimplePrincipal(this.getPrincipalName());
-	        SecurityAssociation.setPrincipal(p);
-	        SecurityAssociation.setCredential(this.getPassword());
+			return fact.create(clazz, url);
 		}
+		catch (MalformedURLException ex) { throw new RuntimeException(ex); }
 	}
 	
 	/** */
 	public Admin getAdmin() throws Exception
 	{
-		this.establish();
-		return this.admin;
+		return (Admin)this.getInterface(Admin.class);
 	}
 	
 	/** */
 	public AccountMgr getAccountMgr()
 	{
-		this.establish();
-		return this.accountMgr;
+		return (AccountMgr)this.getInterface(AccountMgr.class);
 	}
 	
 	/** */
 	public ListMgr getListMgr()
 	{
-		this.establish();
-		return this.listMgr;
+		return (ListMgr)this.getInterface(ListMgr.class);
 	}
 
 	/** */
 	public ListWizard getListWizard()
 	{
-		this.establish();
-		return this.listWizard;
+		return (ListWizard)this.getInterface(ListWizard.class);
 	}
 	
 	/** */
 	public Indexer getIndexer()
 	{
-		this.establish();
-		return this.indexer;
+		return (Indexer)this.getInterface(Indexer.class);
 	}
 	
 	/** */
 	public Injector getInjector()
 	{
-		this.establish();
-		return this.injector;
+		return (Injector)this.getInterface(Injector.class);
 	}
 	
 	/** */
 	public Archiver getArchiver()
 	{
-		this.establish();
-		return this.archiver;
+		return (Archiver)this.getInterface(Archiver.class);
 	}
 	
 	/** */
 	public Plumber getPlumber()
 	{
-		this.establish();
-		return this.plumber;
+		return (Plumber)this.getInterface(Plumber.class);
 	}
 }

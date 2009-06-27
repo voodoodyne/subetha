afafdcae4e14e5d2d99846a9ac350d34258eeb21
Jon Scott Stevens <latchkey@gmail.com>
2009-06-27 00:31:10 +0000

    formatting.
    git-svn-id: https://subetha.googlecode.com/svn/trunk@1325 68df8246-1725-11de-bcda-2bb9902f8029

--- src/org/subethamail/core/util/InjectBeanHelper.java
+++ src/org/subethamail/core/util/InjectBeanHelper.java
@@ -4,29 +4,41 @@ import javax.inject.manager.Manager;
 
 import com.caucho.config.inject.InjectManager;
 
-public class InjectBeanHelper<T> {
+/**
+ *
+ */
+public class InjectBeanHelper<T>
+{
 
 	Manager mgr = InjectManager.create();
-	
-//	@Initializer
-//	public InjectBeanHelper(Manager m){
-//		this.mgr = m;
-//	}
-	
+
+	// @Initializer
+	// public InjectBeanHelper(Manager m){
+	// this.mgr = m;
+	// }
+
 	@SuppressWarnings("unchecked")
-	public T getInstance(String clazz){
+	public T getInstance(String clazz)
+	{
 		T o = null;
 		Class<? extends T> tc = null;
-		try {
+		try
+		{
 			tc = (Class<? extends T>) Class.forName(clazz);
-			o = (T)mgr.getInstanceByType(tc);
-		}catch(ClassNotFoundException e){}
-		
-		if(o==null) o = (T)mgr.getInstanceByName(clazz);
-		
-		return o;		
+			o = this.mgr.getInstanceByType(tc);
+		}
+		catch (ClassNotFoundException e)
+		{
+		}
+
+		if (o == null)
+			o = (T) this.mgr.getInstanceByName(clazz);
+
+		return o;
 	}
-	public T getInstance(Class<? extends T> clazz){
+
+	public T getInstance(Class<? extends T> clazz)
+	{
 		return this.mgr.getInstanceByType(clazz);
 	}
 }

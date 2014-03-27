aee5dc671d949800ba9578a5ab8a57b6dab16901
Jason Pyeron <jpyeron@pdinc.us>
2014-03-26 21:03:37 -0400

    switch logging framework

--- src/org/subethamail/web/action/DeleteMessage.java
+++ src/org/subethamail/web/action/DeleteMessage.java
@@ -8,8 +8,6 @@ package org.subethamail.web.action;
 import lombok.Getter;
 import lombok.Setter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.subethamail.web.Backend;
 import org.subethamail.web.action.auth.AuthAction;
 
@@ -20,10 +18,6 @@ import org.subethamail.web.action.auth.AuthAction;
  */
 public class DeleteMessage extends AuthAction 
 {
-	/** */
-	@SuppressWarnings("unused")
-	private final static Logger log = LoggerFactory.getLogger(DeleteMessage.class);
-	
 	/** */
 	@Getter @Setter Long listId;
 	@Getter @Setter Long msgId;

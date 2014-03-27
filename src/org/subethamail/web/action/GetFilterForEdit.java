4538e41273c80f71096d6b1990d6549b6ef63962
Jason Pyeron <jpyeron@pdinc.us>
2014-03-26 21:03:39 -0400

    switch logging framework

--- src/org/subethamail/web/action/GetFilterForEdit.java
+++ src/org/subethamail/web/action/GetFilterForEdit.java
@@ -5,8 +5,6 @@
 
 package org.subethamail.web.action;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.subethamail.core.plugin.i.FilterParameter;
 import org.subethamail.web.Backend;
 import org.subethamail.web.action.FilterSave.Model;
@@ -20,10 +18,6 @@ import org.subethamail.web.action.auth.AuthAction;
  */
 public class GetFilterForEdit extends AuthAction 
 {
-	/** */
-	@SuppressWarnings("unused")
-	private final static Logger log = LoggerFactory.getLogger(GetFilterForEdit.class);
-	
 	/** */
 	public void initialize()
 	{

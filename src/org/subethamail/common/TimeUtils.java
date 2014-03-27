8d048e22bbf810117206919529d2f24114008065
Jason Pyeron <jpyeron@pdinc.us>
2014-03-27 09:16:19 -0400

    switch logging framework

--- src/org/subethamail/common/TimeUtils.java
+++ src/org/subethamail/common/TimeUtils.java
@@ -8,9 +8,6 @@ package org.subethamail.common;
 import java.sql.Timestamp;
 import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 
 /**
@@ -20,10 +17,6 @@ import org.slf4j.LoggerFactory;
  */
 public class TimeUtils
 {
-	/** */
-	@SuppressWarnings("unused")
-	private final static Logger log = LoggerFactory.getLogger(TimeUtils.class);
-
 	/** default constructor prevents util class from being created. */
 	private TimeUtils() {}
 

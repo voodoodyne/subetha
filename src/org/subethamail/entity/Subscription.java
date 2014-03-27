809ff9b40e609b5be6de593365efbd9e9c9d7c63
Jason Pyeron <jpyeron@pdinc.us>
2014-03-26 21:32:43 -0400

    switch logging framework

--- src/org/subethamail/entity/Subscription.java
+++ src/org/subethamail/entity/Subscription.java
@@ -5,6 +5,8 @@
 
 package org.subethamail.entity;
 
+import java.util.logging.Level;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -12,12 +14,11 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.QueryHint;
-import javax.persistence.Transient;
+
+import lombok.extern.java.Log;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.subethamail.entity.i.Validator;
 
 /**
@@ -75,12 +76,10 @@ import org.subethamail.entity.i.Validator;
 })
 @Entity
 @Cache(usage=CacheConcurrencyStrategy.TRANSACTIONAL)
+@Log
 public class Subscription extends SubscriptionBase
 {
 	private static final long serialVersionUID = 1L;
-
-	/** */
-	@Transient private final static Logger log = LoggerFactory.getLogger(Subscription.class);
 	
 	/** 
 	 * The role of this subscription.
@@ -116,8 +115,7 @@ public class Subscription extends SubscriptionBase
 	
 	public void setRole(Role value)
 	{
-		if (log.isDebugEnabled())
-			log.debug("Setting role to " + value);
+	    log.log(Level.FINE,"Setting role to {0}", value);
 		
 		if (!this.list.isValidRole(value))
 			throw new IllegalArgumentException("Role does not belong to the correct list");
@@ -132,8 +130,7 @@ public class Subscription extends SubscriptionBase
 	
 	public void setNote(String value)
 	{
-		if (log.isDebugEnabled())
-			log.debug("Setting note of " + this + " to " + value);
+	    log.log(Level.FINE,"Setting note of {0} to {1}", new Object[]{this, value});
 		
 		this.note = value;
 	}

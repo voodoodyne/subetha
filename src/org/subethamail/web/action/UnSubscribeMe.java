1de05655b4f56fa1891e90eb8eb94311a59d7783
Jason Pyeron <jpyeron@pdinc.us>
2014-03-26 21:03:46 -0400

    switch logging framework

--- src/org/subethamail/web/action/UnSubscribeMe.java
+++ src/org/subethamail/web/action/UnSubscribeMe.java
@@ -7,9 +7,8 @@ package org.subethamail.web.action;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.java.Log;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.subethamail.web.Backend;
 import org.subethamail.web.action.auth.AuthRequired;
 
@@ -18,12 +17,9 @@ import org.subethamail.web.action.auth.AuthRequired;
  * 
  * @author Jon Stevens
  */
+@Log
 public class UnSubscribeMe extends AuthRequired 
 {
-	/** */
-	@SuppressWarnings("unused")
-	private final static Logger log = LoggerFactory.getLogger(UnSubscribeMe.class);
-	
 	/** */
 	@Getter @Setter Long listId;
 	

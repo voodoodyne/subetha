bd198f72034b55f88cb3a04928a4992f2c6fd92e
Jason Pyeron <jpyeron@pdinc.us>
2014-04-21 12:45:51 -0400

    appserver parameters

--- rtest/src/org/subethamail/rtest/util/WebApp.java
+++ rtest/src/org/subethamail/rtest/util/WebApp.java
@@ -2,7 +2,8 @@ package org.subethamail.rtest.util;
 
 public class WebApp
 {
-	public static String HOSTPORT = "http://localhost:8080";
+    public static String HOSTPORT_PROPERTY=System.getProperty("appserver.url");
+	public static String HOSTPORT = HOSTPORT_PROPERTY==null?"http://localhost:8080":HOSTPORT_PROPERTY;
 	public static String BASEURL = HOSTPORT + "/se";
 
 	public static String getBaseUrl()
